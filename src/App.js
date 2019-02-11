4b79234190514c00260cef7610b06fca80fbb392
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-02-11 14:30:58 +0700

    :sparkles: add styles and fix piano hovering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { parse } from "./library/chord-magic";
 import { chordNotes } from "./library/chord-notes";
-import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "./library/react-piano";
 import { noteNumberToName } from "midiutils";
 import "react-piano/dist/styles.css";
 
 const noteRange = {
   first: MidiNumbers.fromNote("c2"),
-  last: MidiNumbers.fromNote("f5")
+  last: MidiNumbers.fromNote("b3")
 };
 
 const DontTouchContainer = props => {
@@ -32,7 +32,7 @@ function BasicPiano(props) {
 
 class App extends Component {
   state = {
-    currentChord: "C7"
+    currentChord: "Cm7b5"
   };
 
   onChordChange = event => {
@@ -44,14 +44,13 @@ class App extends Component {
   render() {
     const { currentChord } = this.state;
     const { onChordChange } = this;
-    console.log(chordNotes(parse(currentChord)));
     return (
       <div className="App">
         <DontTouchContainer>
           <BasicPiano
             activeNotes={
               parse(currentChord)
-                ? [2, 3, 4]
+                ? [2, 3]
                     .map(octave =>
                       chordNotes(parse(currentChord)).map(e =>
                         MidiNumbers.fromNote(e + octave)
@@ -60,6 +59,7 @@ class App extends Component {
                     .reduce((p, c) => [...p, ...c])
                 : []
             }
+            className="MyCoolPiano"
             renderNoteLabel={({ isActive, midiNumber }) => {
               if (isActive) {
                 const note = noteNumberToName(midiNumber);
@@ -68,8 +68,18 @@ class App extends Component {
             }}
           />
         </DontTouchContainer>
+        <h2> Enter your Chord! </h2>
         <input type="text" value={currentChord} onChange={onChordChange} />
-        <p>{JSON.stringify(parse(currentChord))}</p>
+        <div className="notations">
+          <h3> Notations </h3>
+          <ul>
+            <li> Major 7 th(maj7) </li> <li> Minor 7 th(m7 / -7) </li>
+            <li> Dominant 7 th(7) </li> <li> Half - Dimished 7 th(m7b5) </li>
+            <li> Minor Major(mM7) </li> <li> Root(C / D) </li>
+            <li> Suspended(sus4 / sus2) </li>
+            <li> Added(add9 / add11): Not yet implemented </li>
+          </ul>
+        </div>
       </div>
     );
   }
